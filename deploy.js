d54f0e4844d86d2859162391c0cc20988252a85c
Vandana5s <98863444+Vandana5s@users.noreply.github.com>
2023-02-05 15:02:03 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,6 @@
+console.log("hi");
+//await can be used only in async function, promise based function
+// await waits for one function to be comleted then begins the other
+async function main() {
+  //HTTP://127.0.0.1:7545
+}
